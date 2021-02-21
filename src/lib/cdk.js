7add419fd003658545a6da3645706d0f2c023312
Julian Libor <jl@nuuk.de>
2021-02-21 15:50:30 +0100

    feat: add dynamodb example

---
+++ src/lib/cdk.js
@@ -0,0 +1,18 @@
+const core = require('@aws-cdk/core');
+const dynamodb = require('@aws-cdk/aws-dynamodb');
+
+class DynamoStack extends core.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-dynamodb-readme.html
+    // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.ReadWriteCapacityMode.html
+    new dynamodb.Table(this, 'Items', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Use on-demand billing mode
+      // sortKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    });
+  }
+}
+
+module.exports = { DynamoStack };
